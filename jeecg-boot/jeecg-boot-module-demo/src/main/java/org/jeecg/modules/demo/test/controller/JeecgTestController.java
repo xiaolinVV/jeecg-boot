49f625f9b63bf6a1bbc010703847021fb4302fd3
“zhangshaolin” <zhangshaolin@fjhckj.com>
2022-06-17 23:15:38 +0800

    feat：测试DEMO控制器

--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/test/controller/JeecgTestController.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/test/controller/JeecgTestController.java
@@ -1,5 +1,11 @@
 package org.jeecg.modules.demo.test.controller;
 
+import cn.hutool.core.lang.Dict;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.http.Header;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -28,5 +34,18 @@ public class JeecgTestController {
     @GetMapping(value = "/testLimit")
     public Result<String> testLimit(@RequestParam(name = "orderId", defaultValue = "order1123123") String orderId) {
         return Result.OK("请求成功，orderId：".concat(orderId));
+    }
+
+    public static void main(String[] args) {
+        ThreadUtil.concurrencyTest(30, () -> {
+            String urlStr = "http://127.0.0.1:8080/jeecg-boot/test/testLimit";
+            String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTU0NzgxMzAsInVzZXJuYW1lIjoiYWRtaW4ifQ.YcWTJ8qWw95EhUrsWhPGzLpaPeyoG23qdkwWViIL04A";
+            String result = HttpRequest
+                    .get(urlStr)
+                    .header("X-Access-Token", token, true)
+                    .form(Dict.create().set("orderId", "123456")).execute()
+                    .body();
+            System.out.println(JSONUtil.toJsonPrettyStr(result)) ;
+        });
     }
 }
