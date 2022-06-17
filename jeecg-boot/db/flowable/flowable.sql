61fcb3323f558a6e2d77728bde571027758e7faf
“zhangshaolin” <zhangshaolin@fjhckj.com>
2022-06-17 15:23:33 +0800

    feat：流程引擎 SQL 脚本整理

--- jeecg-boot/db/flowable/flowable.sql
+++ jeecg-boot/db/flowable/flowable.sql
@@ -1,3 +1,33 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for flow_my_business
+-- ----------------------------
+DROP TABLE IF EXISTS `flow_my_business`;
+CREATE TABLE `flow_my_business` (
+                                    `id` varchar(50) COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
+                                    `create_by` varchar(32) COLLATE utf8_general_ci DEFAULT NULL COMMENT '创建人',
+                                    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                    `update_by` varchar(32) COLLATE utf8_general_ci DEFAULT NULL COMMENT '修改人',
+                                    `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+                                    `process_definition_key` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '流程定义key 一个key会有多个版本的id',
+                                    `process_definition_id` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '流程定义id 一个流程定义唯一',
+                                    `process_instance_id` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '流程业务实例id 一个流程业务唯一，本表中也唯一',
+                                    `title` varchar(500) COLLATE utf8_general_ci DEFAULT NULL COMMENT '流程业务简要描述',
+                                    `data_id` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '业务表id，理论唯一',
+                                    `service_impl_name` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '业务类名，用来获取spring容器里的服务对象',
+                                    `proposer` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '申请人',
+                                    `act_status` varchar(100) COLLATE utf8_general_ci DEFAULT NULL COMMENT '流程状态说明，有：启动  撤回  驳回  审批中  审批通过  审批异常',
+                                    `task_id` varchar(1000) COLLATE utf8_general_ci DEFAULT NULL COMMENT '当前的节点定义上的Id,',
+                                    `task_name` varchar(1000) COLLATE utf8_general_ci DEFAULT NULL COMMENT '当前的节点',
+                                    `task_name_id` varchar(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT '当前的节点实例上的Id',
+                                    `todo_users` varchar(1000) COLLATE utf8_general_ci DEFAULT NULL COMMENT '当前的节点可以处理的用户名',
+                                    `done_users` varchar(1000) COLLATE utf8_general_ci DEFAULT NULL COMMENT '处理过的人',
+                                    `priority` varchar(100) COLLATE utf8_general_ci DEFAULT NULL COMMENT '当前任务节点的优先级 流程定义的时候所填',
+                                    PRIMARY KEY (`id`) /*T![clustered_index] NONCLUSTERED */,
+                                    UNIQUE KEY `dataid` (`data_id`) COMMENT '业务数据Id索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='流程业务扩展表';
 
 -- 流程状态
 INSERT INTO `sys_dict`(`id`, `dict_name`, `dict_code`, `description`, `del_flag`, `create_by`, `create_time`, `update_by`, `update_time`, `type`) VALUES ('1533795346018693121', '流程状态说明', 'act_status', '流程状态说明', 0, 'admin', '2022-06-06 20:58:02', NULL, NULL, 0);
@@ -17,3 +47,18 @@ INSERT INTO `sys_permission`(`id`, `parent_id`, `name`, `url`, `component`, `com
 INSERT INTO `sys_permission`(`id`, `parent_id`, `name`, `url`, `component`, `component_name`, `redirect`, `menu_type`, `perms`, `perms_type`, `sort_no`, `always_show`, `icon`, `is_route`, `is_leaf`, `keep_alive`, `hidden`, `hide_tab`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `del_flag`, `rule_flag`, `status`, `internal_or_external`) VALUES ('1529772403697627137', '1529772027745382402', '流程设计', '/flowable/modeler/modelerDesign', 'flowable/modeler/modelerDesign', NULL, NULL, 1, NULL, '1', 1, 0, NULL, 1, 1, 0, 0, 0, NULL, 'admin', '2022-05-26 18:32:17', 'admin', '2022-06-05 18:29:45', 0, 0, '1', 0);
 INSERT INTO `sys_permission`(`id`, `parent_id`, `name`, `url`, `component`, `component_name`, `redirect`, `menu_type`, `perms`, `perms_type`, `sort_no`, `always_show`, `icon`, `is_route`, `is_leaf`, `keep_alive`, `hidden`, `hide_tab`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `del_flag`, `rule_flag`, `status`, `internal_or_external`) VALUES ('1534462289604214786', '1534453116317429762', '待办任务', '/flowable/todoList/MyTodoList', 'flowable/todoList/MyTodoList', NULL, NULL, 1, NULL, '1', 1, 0, NULL, 1, 1, 0, 0, 0, NULL, 'admin', '2022-06-08 17:08:13', NULL, NULL, 0, 0, '1', 0);
 INSERT INTO `sys_permission`(`id`, `parent_id`, `name`, `url`, `component`, `component_name`, `redirect`, `menu_type`, `perms`, `perms_type`, `sort_no`, `always_show`, `icon`, `is_route`, `is_leaf`, `keep_alive`, `hidden`, `hide_tab`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `del_flag`, `rule_flag`, `status`, `internal_or_external`) VALUES ('1534473146522349569', '1534472925054709762', '请假申请', '/demo/tbLeaveApplyList/TbLeaveApplyList', 'demo/tbLeaveApplyList/TbLeaveApplyList', NULL, NULL, 1, NULL, '1', 1, 0, NULL, 1, 1, 0, 0, 0, NULL, 'admin', '2022-06-08 17:51:22', 'admin', '2022-06-08 17:54:54', 0, 0, '1', 0);
+
+-- 流程菜单默认授权给 admin
+INSERT INTO `sys_role_permission`(`id`, `role_id`, `permission_id`, `data_rule_ids`, `operate_date`, `operate_ip`) VALUES ('1534453853265031170', 'f6817f48af4fb3af11b9e8bf182f618b', '1534453758712836097', NULL, '2022-06-08 16:34:42', '0:0:0:0:0:0:0:1');
+INSERT INTO `sys_role_permission`(`id`, `role_id`, `permission_id`, `data_rule_ids`, `operate_date`, `operate_ip`) VALUES ('1534462598825082881', 'f6817f48af4fb3af11b9e8bf182f618b', '1534462289604214786', NULL, '2022-06-08 17:09:27', '0:0:0:0:0:0:0:1');
+INSERT INTO `sys_role_permission`(`id`, `role_id`, `permission_id`, `data_rule_ids`, `operate_date`, `operate_ip`) VALUES ('1529772484307955713', 'f6817f48af4fb3af11b9e8bf182f618b', '1529772403697627137', NULL, '2022-05-26 18:32:37', '0:0:0:0:0:0:0:1');
+INSERT INTO `sys_role_permission`(`id`, `role_id`, `permission_id`, `data_rule_ids`, `operate_date`, `operate_ip`) VALUES ('1529774723948851201', 'f6817f48af4fb3af11b9e8bf182f618b', '1529774634329157633', NULL, '2022-05-26 18:41:31', '0:0:0:0:0:0:0:1');
+INSERT INTO `sys_role_permission`(`id`, `role_id`, `permission_id`, `data_rule_ids`, `operate_date`, `operate_ip`) VALUES ('1534463722533629953', 'f6817f48af4fb3af11b9e8bf182f618b', '1534463621773864962', NULL, '2022-06-08 17:13:55', '0:0:0:0:0:0:0:1');
+
+-- ----------------------------
+-- Records of flow_my_business
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
