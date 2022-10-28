2ada7234540dc969379164e082d0e746b68ed5d2
张少林 <1083065285@qq.com>
2022-10-28 16:01:51 +0800

    feat: only test push

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/demo/tbLeaveApply/service/ITbLeaveApplyService.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/demo/tbLeaveApply/service/ITbLeaveApplyService.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 /**
  * @Description: 请假申请
  * @Author: jeecg-boot
- * @Date:   2022-10-28
+ * @Date:   2022-10-27
  * @Version: V1.0
  */
 public interface ITbLeaveApplyService extends IService<TbLeaveApply> {
