4b94379acdea3e2295e94bcf959b68833e681782
张少林 <1083065285@qq.com>
2022-12-09 15:56:01 +0800

    feat: 修复查询报错

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/member/mapper/MemberDesignationMemberListMapper.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/member/mapper/MemberDesignationMemberListMapper.java
@@ -3,6 +3,7 @@ package org.jeecg.modules.member.mapper;
 import java.util.List;
 import java.util.Map;
 
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
@@ -20,10 +21,13 @@ import org.jeecg.modules.member.vo.MemberDesignationMemberListVO;
  */
 public interface MemberDesignationMemberListMapper extends BaseMapper<MemberDesignationMemberList> {
 
+    @InterceptorIgnore(tenantLine = "true")
     List<Map<String,Object>> findMemberDesignationList(@Param("memberId") String memberId);
 
+    @InterceptorIgnore(tenantLine = "true")
     IPage<MemberDesignationMemberListVO> queryPageList(Page<MemberDesignationMemberListVO> page,@Param(Constants.WRAPPER)QueryWrapper<MemberDesignationMemberListVO> queryWrapper);
 
+    @InterceptorIgnore(tenantLine = "true")
     List<MemberDesignationMemberListVO> findMemberDesignationMemberlist(@Param("memberDesignationMemberListVO") MemberDesignationMemberListVO memberDesignationMemberListVO);
 
 }
