a40f18fce9ba6be0cd7993d63800c11c7670ed95
张少林 <1083065285@qq.com>
2023-01-11 09:57:58 +0800

    feat: 预览图片&下载文件 接口支持多路径，兼容前端旧代码

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/CommonController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/CommonController.java
@@ -207,7 +207,7 @@ public class CommonController {
      * @param request
      * @param response
      */
-    @GetMapping(value = "/static/**")
+    @GetMapping(value = {"/static/**","/view/**"})
     public void view(HttpServletRequest request, HttpServletResponse response) {
         // ISO-8859-1 ==> UTF-8 进行编码转换
         String imgPath = extractPathFromPattern(request);
