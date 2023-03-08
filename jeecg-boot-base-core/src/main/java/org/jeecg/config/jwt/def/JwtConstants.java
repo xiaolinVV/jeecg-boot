e5efd79733078f5fcc08a5b57f033dd71628a248
张少林 <1083065285@qq.com>
2023-03-08 22:49:33 +0800

    feat: 修改签名秘钥串、token 签名秘钥

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/jwt/def/JwtConstants.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/jwt/def/JwtConstants.java
@@ -5,7 +5,7 @@ public class JwtConstants {
 	/**
 	 * 上线需要变更
 	 */
-	public static final String JWT_SECRET = "JYJ5Qv2WF4lA6jPl5GKuAG";
+	public static final String JWT_SECRET = "D13TLSAxl8JcGleRcMZLYeEA";
 	
 	public static final String AUTHORIZATION = "X-AUTH-TOKEN";
 	public static final String CURRENT_USER_NAME = "memberId";
