fef08a7c43a175d96dfb8229924667ff3d149b4b
张少林 <1083065285@qq.com>
2024-07-16 11:45:43 +0800

    feat： dify http 节点测试

--- jeecg-module-demo/src/main/java/org/jeecg/modules/demo/test/controller/JeecgDemoController.java
+++ jeecg-module-demo/src/main/java/org/jeecg/modules/demo/test/controller/JeecgDemoController.java
@@ -488,7 +488,7 @@ public class JeecgDemoController extends JeecgController<JeecgDemo, IJeecgDemoSe
 
 
     @PostMapping("/dify/chat")
-    public Result<?> difyChat(@RequestBody JSONObject jsonObject){
+    public Result<?> difyChat(JSONObject jsonObject){
         log.info("dify 聊天数据：{}",jsonObject.toJSONString());
         return Result.OK(jsonObject);
     }
