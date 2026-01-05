9ec1d45eca7b8c6d3026ef598e8c4dd2f354c4f3
xiaolinV01 <repair-jinx-growl@duck.com>
2026-01-05 14:43:32 +0800

    ``` fix: 修正 SysRoleIndex 实体类 route 字段类型为 Boolean
    - 将 is_route 字段类型从 String 改回 Boolean
    - 确保字段类型与数据库表定义保持一致
    - 修正字段类型映射，避免类型转换异常
    ```

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/entity/SysRoleIndex.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/entity/SysRoleIndex.java
@@ -50,7 +50,7 @@ public class SysRoleIndex {
 	@Excel(name = "是否路由菜单", width = 15)
 	@ApiModelProperty(value = "是否路由菜单")
 	@TableField(value="is_route")
-	private String route;
+	private Boolean route;
 	/**优先级*/
 	@Excel(name = "优先级", width = 15)
     @ApiModelProperty(value = "优先级")
